9aa33979506b6dc3578bc583c5979cb94573a2f1
Ivan Kuzmenko <6745157+rndtrash@users.noreply.github.com>
2025-06-14 19:46:05 +0300

    Add test to GitHub CI

---
+++ tests/simulator.test.ts
@@ -0,0 +1,13 @@
+import { describe, it, expect } from 'vitest';
+
+describe('simulator', () => {
+    it('runs with default setup', () => {
+        expect(true).toBe(true);
+    });
+    it('can tick', () => {
+        expect(true).toBe(true);
+    });
+    it('can load ROM from file', () => {
+        expect(true).toBe(true);
+    });
+});
