80bef56ab85fee831cb8c7c715ce48c5ecf0302f
Ivan Kuzmenko <6745157+rndtrash@users.noreply.github.com>
2025-06-25 09:34:11 +0300

    Fix tests

--- tests/simulator.test.ts
+++ tests/simulator.test.ts
@@ -1,13 +1,20 @@
 import { describe, it, expect } from 'vitest';
+import { Machine } from 'src/backend/Emulator/Machine';
+import { ZeroToZero } from 'src/backend/Emulator/Masters/ZeroToZero';
+import { ConsoleLog } from 'src/backend/Emulator/Devices/ConsoleLog';
 
 describe('simulator', () => {
+	let machine: Machine;
     it('runs with default setup', () => {
-        expect(true).toBe(true);
+        expect(() => {
+        	machine = Machine.FromMasterDevices(ZeroToZero.fromContext(undefined), [ConsoleLog.fromContext(undefined)]);
+        }).not.toThrowError();
     });
     it('can tick', () => {
-        expect(true).toBe(true);
-    });
-    it('can load ROM from file', () => {
-        expect(true).toBe(true);
+        expect(() => {
+        	for (const i of Array(10)) {
+        		machine.doTick();
+        	}
+        }).not.toThrowError();
     });
 });
